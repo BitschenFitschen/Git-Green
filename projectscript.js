4c31bd76bfca4e3005f252872ef5ec6b5cf98581
Ron Johnson <ronjohnson@MacBook-Air.local>
2017-03-21 21:54:44 -0700

    image and title append

--- projectscript.js
+++ projectscript.js
@@ -9,10 +9,23 @@ var APIURL = "http://webhose.io/search?token=f613b0c1-4567-4751-8760-c3da580bc11
 	$.ajax({
       url: APIURL,
       method: "GET"
-    }).done(function(response){
+
+    })
+
+    .done(function(response){
     	console.log(response)
     
+    var shortenedArray = response.posts.slice(95,99);
+    for(var i = 0; i < shortenedArray.length; i++) {
+    	console.log(shortenedArray[i].title)
+        $('#ask').append('<span>' + shortenedArray[i].title + '</span>')
+    }
 
-    $("#ask").html(response.author);
+	var shortenedArray = response.posts.slice(95, 99);
+        for (var i = 0; i < shortenedArray.length; i++) {
+            console.log(shortenedArray[i].thread.main_image)
+            $('#bid').append('<img src = "' + shortenedArray[i].thread.main_image + '"/>')
+        }
+
 
   });
